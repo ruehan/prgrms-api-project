f03caa0f0007f656c64f9505035a2db6210dd361
ruehan <node.js_@kakao.com>
2024-09-24 16:39:57 +0900

    ✨ Route 기본 구조 생성
    Route 기본 구조 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import { Provider } from "react-redux";
-import store from "./store";
-import MainPage from "./components/MainPage";
+import React from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
+import MainPage from './components/MainPage'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import SubPage from './components/SubPage'
 
 const App: React.FC = () => {
-	return (
-		<Provider store={store}>
-			<MainPage />
-		</Provider>
-	);
-};
+  return (
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/sub" element={<SubPage />}></Route>
+        </Routes>
+      </Router>
+    </Provider>
+  )
+}
 
-export default App;
+export default App

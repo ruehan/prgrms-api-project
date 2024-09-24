c3c4c8ecfa094a1a412a38060034349127e31316
ruehan <node.js_@kakao.com>
2024-09-24 16:39:16 +0900

    ✨ 페이지 생성
    페이지 생성

--- src/components/SubPage.tsx
+++ src/components/SubPage.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../types'
 
-const MainPage: React.FC = () => {
+const SubPage: React.FC = () => {
   const greeting = useSelector((state: RootState) => state.greeting.message)
 
   return (
     <div>
-      <h1>{greeting + ' 메인 페이지 입니다.'}</h1>
+      <h1>{greeting + ' 서브 페이지 입니다.'}</h1>
     </div>
   )
 }
 
-export default MainPage
+export default SubPage

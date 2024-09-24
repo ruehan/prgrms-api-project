172687fdcd920bbf0488b2f211f3daa15403ff60
ruehan <node.js_@kakao.com>
2024-09-24 16:37:22 +0900

    🚚 디렉토리 이름 변경 -> modules
    디렉토리 이름 변경 -> modules

--- src/ducks/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import greetingReducer from "./greeting";
-
-const rootReducer = combineReducers({
-	greeting: greetingReducer,
-});
-
-export type RootState = ReturnType<typeof rootReducer>;
-export default rootReducer;
---
+++ src/modules/index.ts
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+import greetingReducer from './greeting'
+
+const rootReducer = combineReducers({
+  greeting: greetingReducer,
+})
+
+export type RootState = ReturnType<typeof rootReducer>
+export default rootReducer

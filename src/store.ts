14e3abc8692f2fca0a69a36eb4d19a2f4e254464
ruehan <node.js_@kakao.com>
2024-09-27 16:39:59 +0900

    🐛 누락된 코드 업데이트
    누락된 코드 업데이트

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,6 @@
-import { createStore } from "redux";
-import rootReducer from "./ducks";
+import { createStore } from 'redux'
+import rootReducer from './modules'
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer)
 
-export default store;
+export default store

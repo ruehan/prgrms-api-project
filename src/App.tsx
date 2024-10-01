a536666ad28fd4556c7b84ecb712e1daf2c40cfe
ruehan <node.js_@kakao.com>
2024-10-01 10:40:40 +0900

    ✨ Route 설정
    Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,27 @@ import { QueryClient, QueryClientProvider } from 'react-query'
 import MenuBar from './components/global/menubar'
 import 'leaflet/dist/leaflet.css'
 import NearbyPerformanceFacilities from './components/nearby/page'
+import RecommendedShows from './components/recommend/page'
+import { AuthProvider } from './components/recommend/AuthContext'
 
 const App: React.FC = () => {
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
-      <Provider store={store}>
-        <Router>
-          <MenuBar />
-          <Routes>
-            <Route path="/" element={<MainPage />}></Route>
-            <Route path="/search" element={<SearchPage />}></Route>
-            <Route
-              path="/nearby"
-              element={<NearbyPerformanceFacilities />}
-            ></Route>
-          </Routes>
-        </Router>
-      </Provider>
+      <AuthProvider>
+        <Provider store={store}>
+          <Router>
+            <MenuBar />
+            <Routes>
+              <Route path="/" element={<MainPage />}></Route>
+              <Route path="/search" element={<SearchPage />}></Route>
+              <Route path="/nearby" element={<NearbyPerformanceFacilities />}></Route>
+              <Route path="/recommend" element={<RecommendedShows />}></Route>
+            </Routes>
+          </Router>
+        </Provider>
+      </AuthProvider>
     </QueryClientProvider>
   )
 }
